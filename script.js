4b95a38d283ce39e443fef7da96d3dac71534611
Abin John <abinjohnanto@gmail.com>
2022-11-11 21:00:13 +0530

    feat: implement time input logic

---
+++ script.js
@@ -0,0 +1,31 @@
+// ##################################
+// auto focus input fields
+// for time input
+// ##################################
+
+const timeInputsContainer = document.querySelector('.adjustment-inputs');
+
+function timeInputHandler(e) {
+  // check if input is valid
+  const inputValue = e.target.value;
+  if (!inputValue) {
+    e.target.value = '';
+    return;
+  }
+
+  // shift focus to next input element
+  const currentInputEl = Number.parseInt(e.target.dataset.time);
+  const nextInputEl = document.querySelector(
+    `[data-time="${currentInputEl + 1}"]`
+  );
+  if (!nextInputEl) return;
+  nextInputEl.select();
+}
+
+// select input value when focused
+function timeFocusHandler(e) {
+  e.target.select();
+}
+
+timeInputsContainer.addEventListener('input', timeInputHandler);
+timeInputsContainer.addEventListener('focusin', timeFocusHandler);
