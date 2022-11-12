73930141e173872e274c2511afe6db9e82465088
Abin John <abinjohnanto@gmail.com>
2022-11-12 18:10:09 +0530

    feat: add set adjustment type logic

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-const timeInputsContainer = document.querySelector('.adjustment-inputs');
+// State variables
+let adjustmentType = -1;
 
 // ##################################
 // auto focus input fields
 // for time input
 // ##################################
+const timeInputsContainer = document.querySelector('.adjustment-inputs');
 
 function timeInputHandler(e) {
   // check if input is valid
@@ -39,8 +41,24 @@ timeInputsContainer.addEventListener('focusin', timeFocusHandler);
 const btnClear = document.querySelector('.btn-clear');
 
 function clearInputs() {
-  const inputs = timeInputsContainer.querySelectorAll('input[type="number"]');
+  const inputs = document.querySelectorAll('input[type="number"]');
   inputs.forEach((input) => (input.value = ''));
 }
 
 btnClear.addEventListener('click', clearInputs);
+
+// ##################################
+// select adjustment type
+// ##################################
+const adjustmentBtns = document.querySelector('.adjustment-controls');
+
+function setAdjustmentType(e) {
+  const targetBtn = e.target.closest('.btn-control');
+  adjustmentType = Number.parseInt(targetBtn.value);
+
+  adjustmentBtns
+    .querySelectorAll('.btn-control')
+    .forEach((btn) => btn.classList.toggle('--selected', btn === targetBtn));
+}
+
+adjustmentBtns.addEventListener('click', setAdjustmentType);
